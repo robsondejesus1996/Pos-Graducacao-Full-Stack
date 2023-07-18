d6a577cadffe69e84c0812706dceb4765113938a
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-07-18 11:21:56 -0300

    Modulo de JS

---
+++ JavaScript/prog01.js
@@ -0,0 +1,27 @@
+let nome = 'Robson';
+console.log(nome)
+
+
+let idade = 50;
+console.log(idade)
+
+
+console.log('-----------------------------------')
+{
+    let nome2 = 'Marcos';
+    console.log(nome2)
+}
+//console.log(nome2)
+console.log('-----------------------------------')
+
+{
+    var nome2 = 'Marcos';
+    console.log(nome2)
+}
+console.log(nome2)
+
+console.log('-----------------------------------')
+{
+    const PI = 3.14159
+    console.log(PI)
+}
---
+++ JavaScript/prog02.js
@@ -0,0 +1,36 @@
+let v;
+v = 'JavaScript'
+console.log(v)
+
+v = 31
+console.log(v)
+
+console.log('---------------------------------')
+let nome = 'Marcos'
+let caado = true;
+let filhos = 3
+
+console.log(typeof nome)
+console.log(typeof caado)
+console.log(typeof filhos)
+console.log('---------------------------------')
+
+console.log('Tipos basicos de dados')
+v = 25893450934502734059723845789347895n;
+console.log(v)
+
+
+console.log('---------------------------------')
+let nome2 = 'Robson'
+let mensagem = 
+`<p>
+    Ola ${nome2}
+</p>`;
+console.log(mensagem)
+console.log(typeof mensagem)
+
+
+console.log('---------------------------------')
+
+let a = 3, b = 5;
+console.log(`A soma de ${a} e ${b} e igual a ${a+b}`)
