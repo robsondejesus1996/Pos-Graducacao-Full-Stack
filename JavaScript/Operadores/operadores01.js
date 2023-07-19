0eaa4027d2eafc0e9d85ffc8114fde439ac11108
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-07-19 09:12:50 -0300

    Aula operadores

---
+++ JavaScript/Operadores/operadores01.js
@@ -0,0 +1,23 @@
+let n = +"12";
+console.log(1, n, typeof n);
+
+
+console.log("------------------------------------")
+
+let a = 3;
+console.log(--a *2, a);
+
+console.log("------------------------------------")
+console.log("Coercao")
+let a1 = "5" + 2 
+console.log(a1)
+
+console.log("------------------------------------")
+
+
+let nome = ""
+if(nome){
+    console.log(nome)
+}else{
+    console.log('nome vazio')
+}
