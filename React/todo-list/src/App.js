fe164ad6daf1e48552063e2e7e33051f6fb849a3
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-04-30 11:20:46 -0300

    React hooks e criação projeto todolist

---
+++ React/todo-list/src/App.js
@@ -0,0 +1,12 @@
+import { TodoList } from "./components/TodoList";
+
+function App() {
+  return (
+    <section>
+      <h1>Custom Todo-List</h1>
+      <TodoList />
+    </section>
+  );
+}
+
+export default App;
---
+++ React/todo-list/src/components/TodoListItem.js
@@ -0,0 +1,8 @@
+export const TodoListItem = (props) => {
+    return(
+        <li>
+            <input type="checkbox" />
+            {props.item.name}
+        </li>
+    )
+}
